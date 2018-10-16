68dac0378f39f98ae8f55c02b1d9e20ee165476f
Alvaro Moran <alvaro.moran@gmail.com>
2018-10-16 15:49:16 +0200

    added other fixes

--- fixer.py
+++ fixer.py
@@ -21,8 +21,11 @@ def get_files():
             not f.startswith('./www/.git')]
 
 def get_0xfcc4(files):
-    out = subprocess.run("ag -l _0xfcc4 " + os.getcwd() + "/www", shell=True, capture_output=True)
-    matches = [f.decode(sys.stdout.encoding) for f in out.stdout.strip().split(b'\n') if f != b""]
+    try:
+        out = subprocess.check_output("rg -l _0xfcc4 " + os.getcwd() + "/www", shell=True)
+    except subprocess.CalledProcessError:
+        return []
+    matches = [f.decode(sys.stdout.encoding) for f in out.strip().split(b'\n') if f != b""]
     """
     In alternative you can run this, but in my mac it's slower :(
     for fn in files:
@@ -44,8 +47,11 @@ def fix_0xfcc4(files):
             f.write(txt2)
 
 def get_118(files):
-    out = subprocess.run("ag -l 'String.fromCharCode\\(118' " + os.getcwd() + "/www", shell=True, capture_output=True)
-    return [f.decode(sys.stdout.encoding) for f in out.stdout.strip().split(b'\n') if f != b""]
+    try:
+        out = subprocess.check_output("rg -l 'String.fromCharCode\\(118' " + os.getcwd() + "/www", shell=True)
+    except subprocess.CalledProcessError:
+        return []
+    return [f.decode(sys.stdout.encoding) for f in out.strip().split(b'\n') if f != b""]
 
 def fix_118(files):
     txt = None
@@ -56,6 +62,52 @@ def fix_118(files):
         txt2 = re.sub(r"eval\(String\.fromCharCode\(118.*\)\);", "", txt1, flags=re.M)
         with open(fn, 'w') as f:
             f.write(txt2)
+
+def get_suspicious_eval(files):
+    wplist = None
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'files-wordpress-4.9.3.txt'), 'r') as f:
+        wplist = f.readlines()
+    #print(wplist[0].encoding)
+
+    try:
+        out = subprocess.check_output("rg -l 'eval\\s*\\(' " + os.getcwd() + "/www", shell=True)
+    except subprocess.CalledProcessError:
+        return []
+    return [f.decode(sys.stdout.encoding) for f in out.strip().split(b'\n')
+                if f != b"" and
+                f.decode(sys.stdout.encoding).replace(os.getcwd() + "/www/", "") not in wplist]
+
+def get_suspisious_php_code(files):
+    try:
+        out = subprocess.check_output("rg -l '<\\?php\\s\\s+\\$\\w+\\s=\\s\\d+;' " + os.getcwd() + "/www", shell=True)
+    except subprocess.CalledProcessError:
+        return []
+    return [f.decode(sys.stdout.encoding) for f in out.strip().split(b'\n') if f != b""]
+
+def fix_suspicious_php_code(files):
+    txt = None
+    for fn in files:
+        with open(fn, 'r', errors='ignore') as f:
+            txt = f.read()
+        txt1 = re.sub(r"<\?php\s\s+\$\w+\s=\s\d+;.*\?>", "", txt, flags=re.M)
+        with open(fn, 'w') as f:
+            f.write(txt1)
+
+def get_suspisious_php_include(files):
+    try:
+        out = subprocess.check_output("rg -l '@include \"' " + os.getcwd() + "/www", shell=True)
+    except subprocess.CalledProcessError:
+        return []
+    return [f.decode(sys.stdout.encoding) for f in out.strip().split(b'\n') if f != b""]
+
+def fix_suspicious_php_include(files):
+    txt = None
+    for fn in files:
+        with open(fn, 'r', errors='ignore') as f:
+            txt = f.read()
+        txt1 = re.sub(r"@include\s\".*;", "", txt, flags=re.M)
+        with open(fn, 'w') as f:
+            f.write(txt1)
 
 def main():
     """Main entry function."""
@@ -73,6 +125,24 @@ def main():
     fix_118(matches)
     matches = get_118(files)
     print("Matches of 118 now: {}".format(len(matches)))
+
+    matches = get_suspisious_php_code(files)
+    print("Number of suspicious php code matches: {}".format(len(matches)))
+    fix_suspicious_php_code(matches)
+    matches = get_suspisious_php_code(files)
+    print("Matches of suspicious php code now: {}".format(len(matches)))
+
+    matches = get_suspisious_php_include(files)
+    print("Number of suspicious php includes matches: {}".format(len(matches)))
+    fix_suspicious_php_include(matches)
+    matches = get_suspisious_php_include(files)
+    print("Matches of suspicious php includes now: {}".format(len(matches)))
+
+    matches = get_suspicious_eval(files)
+    print("Matches of suspicious eval: {}".format(len(matches)))
+    print("\nSuspicious eval matches, to edit or delete manually:\n\n{}".format("\n".join(matches)))
+    print("\nTo finish, compare a reference wordpress with the one attacked to see if there are still bogus files.")
+
 
 if __name__ == "__main__":
     main()
